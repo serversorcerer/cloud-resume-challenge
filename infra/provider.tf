e4355bbd3d84d52a1bb807c2d48f7a694f17db14
serversorcerer <joe@josephaleto.iom>
2025-05-28 20:26:06 -0400

    Full website + Terraform infra finalized with v1.12.1

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,15 @@
- provider "aws" {
+terraform {
+  required_version = ">= 1.12.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.98.0"
+    }
+  }
+}
+
+provider "aws" {
   region = "us-east-1"
 }
 
