27f3e83ef767077890a04977c9d640b718359940
Joseph Leto <134334331+serversorcerer@users.noreply.github.com>
2025-05-29 17:55:43 -0400

    Fix command parsing and add command API module (#10)

--- infra/modules/command_api/variables.tf
+++ infra/modules/command_api/variables.tf
@@ -25,3 +25,13 @@ variable "timeout" {
   type        = number
   default     = 5
 }
+
+variable "function_name" {
+  description = "Lambda function name"
+  type        = string
+}
+
+variable "lambda_source_path" {
+  description = "Path to Lambda source file"
+  type        = string
+}
