bf6fcbde41fe898c35cb3f03c0705f5b7a1eee2f
serversorcerer <joe@josephaleto.iom>
2025-05-28 20:09:25 -0400

    Add infra folder with Terraform setup

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,15 @@
- 
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "domain_name" {
+  description = "Domain name for your site (e.g., josephaleto.io)"
+  type        = string
+}
+
+variable "hosted_zone_id" {
+  description = "Route 53 hosted zone ID for DNS records"
+  type        = string
+}
