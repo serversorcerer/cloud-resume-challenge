9edcf4edcdbebc2e3305d32e54f1fdeabe7112cb
Joe Leto <134334331+serversorcerer@users.noreply.github.com>
2025-07-11 20:53:25 -0400

    Update constants.js configuration

--- lambda/lib/constants.js
+++ lambda/lib/constants.js
@@ -6,7 +6,7 @@ module.exports = {
   WIN_PAYOUT: 2, // bet + 1x
   PUSH_PAYOUT: 1, // return bet
   SURRENDER_PAYOUT: 0.5, // return half bet
-  INSURANCE_PAYOUT: 3, // bet returned plus 2:1
+  INSURANCE_PAYOUT: 2, // bet returned plus 2:1
   NUM_DECKS: 8, // Use 8 decks (52x8 cards)
   MAX_SPLITS: 4
 };
